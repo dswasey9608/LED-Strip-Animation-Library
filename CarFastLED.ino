4ac3be24c45a960f4ca30a56bd7566d024805055
BaconSD <dswasey9608@gmail.com>
2019-12-23 23:31:34 -0700

    Splosions!
    Added a pseudo-random explosion animation. There are still some kinks in it that can be found in the code for the 'explosions' function. This can be fixed by changing which way the leds are iterated over depending on if it is determined that the loop will overshoot the end of the array or the beginning of it.

--- CarFastLED.ino
+++ CarFastLED.ino
@@ -12,11 +12,12 @@
 
 CRGBArray<NUM_LEDS> leds;
 CRGBArray<NUM_LEDS> newColor;
-const int cycles = 5;
+const int cycles = 6;
 //int interruptPin = D0; //see image in current directory (BaconSD C://Documents/Arduino/FastLEDProjects) to see Arduino's pinout mapping.
 
 void setup() { 
   FastLED.addLeds<WS2812,3>(leds, NUM_LEDS);
+  srand(time(NULL));
   FastLED.setMaxRefreshRate(100);
   //pinMode(interruptPin, INPUT_PULLUP);
   //attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, FALLING);
@@ -24,10 +25,11 @@ void setup() {
 }
 
 void loop(){
-//  scrollingRainbow(cycles);
-//  breathing(cycles);
+//  scrollingRainbow();
 //  stripeScroll();
-  stackLights();
+//  stackLights();
+//  breathing();
+  randExplosions();
 }
 
 //ICACHE_RAM_ATTR void handleInterrupt(){
@@ -46,63 +48,21 @@ void scrollingRainbow(){
   scroll();
 }
 
-void scroll(){
-  newColor = leds; //make copy of initial values
-  int numCycles = 0;
-  int j = 1;
-  while(1){
-    if(j == NUM_LEDS + 1){//if j becomes greater than the amount of LEDS, reset to 1 to take from beginning of array again
-    j = 1;
-    numCycles++;
-    if(numCycles == 10){
-      return;
-    }
-  }
-  for(int i = 0; i <= NUM_LEDS; i++){
-    int diff = i - j;
-    if(diff < 0){ //if the difference is below zero, need to access element that is 'diff' less than number of leds
-      leds[i] = newColor[NUM_LEDS - abs(diff)];
-    }
-    else{
-      leds[i] = newColor[i - j]; //if the difference isn't less than 0 (EX: i = 10, j = 2), simply grab the correct color.
-    }
-  }
-    leds(NUM_LEDS/2,NUM_LEDS-1) = leds(NUM_LEDS/2 - 1 ,0);
-    FastLED.show();
-    FastLED.delay(20);
-    j++; //increment j to reach the next static color
-  }
-}
-
 void breathing(){ //cycles between all primary and secondary colors
   int powerScale = 0;
   int increment = 1;
   bool upIncrement = true;
   int colorNumber = 0;
   int numCycles = 0;
+  bool firstCycle = true;
   while(1){
-    for(int b = 0; b <= NUM_LEDS; b++){
-      switch(colorNumber){
-        case 0: leds[b] = CRGB(0,100*(powerScale*.01),0);//red
-                break;
-        case 1: leds[b] = CRGB(100*(powerScale*.01),100*(powerScale*.01),0);
-                break;
-        case 2: leds[b] = CRGB(100*(powerScale*.01),0,0);//green
-                break;
-        case 3: leds[b] = CRGB(100*(powerScale*.01),0,100*(powerScale*.01));
-                break;
-        case 4: leds[b] = CRGB(0,0,100*(powerScale*.01));//blue
-                break;
-        case 5: leds[b] = CRGB(0,100*(powerScale*.01),100*(powerScale*.01));
-                break;
-        default: leds[b] = CRGB(0,0,100*(powerScale*.01));
-      }
-      // //red
-      // //green
-       //blue
-      //leds[b] = CRGB(127, (int)255*(powerScale*.02), (int)212*(powerScale*.02)); 
+    if(numCycles == cycles + 1){
+      return;
     }
-    
+    CRGB color = colorSelector(colorNumber, powerScale);
+    for(int b = 0; b <= NUM_LEDS; b++){
+        leds[b] = color;
+    }
     FastLED.show();
     //Serial.println("Value of powerScale is: ");
     //Serial.println(powerScale);
@@ -111,7 +71,9 @@ void breathing(){ //cycles between all primary and secondary colors
       upIncrement = false;
     }
     if(powerScale == 0){
-      colorNumber++;
+      if(!firstCycle){
+        colorNumber++;
+      }
       if(colorNumber >= 6){
         colorNumber = 0;
       }
@@ -129,10 +91,7 @@ void breathing(){ //cycles between all primary and secondary colors
     else{
       powerScale = powerScale - increment;
     }
-
-    if(numCycles == cycles){
-      return;
-    }
+    firstCycle = false;
    
   }
 }
@@ -254,7 +213,57 @@ void stackLights(){
     }
   }
   scroll();
-  
+} 
+
+//void ripple(){
+//  
+//}
+
+
+void randExplosions(){
+  int numOfExplosions = 0;
+  int ledPosition;
+  bool doneWithSplodeys = false;
+  while(!doneWithSplodeys){
+    ledPosition = rand() % 150;
+      explosion(10, ledPosition, colorSelector(rand() % 5, 150)); //make an explosion over the whole strip
+      for(int i = 0; i < 15; i++){
+        leds.fadeToBlackBy(100);
+        FastLED.show();
+        FastLED.delay(50); 
+      }
+      FastLED.delay(1000);
+      numOfExplosions++;
+      if(numOfExplosions == cycles){
+        doneWithSplodeys = true;
+      }
+  }
+}
+
+void explosion(int expSize, int ledPosition, CRGB color){
+  int above = ledPosition + expSize;
+  int below = ledPosition - expSize;
+  if(expSize % 2 == 0){
+    expSize++; //make size odd so explosion can be symmetrical from a single central LED
+  }
+  for(int i = ledPosition; i < ledPosition + expSize; i++){
+    leds.fadeToBlackBy(100);
+    if(i <= NUM_LEDS && i >= 0){
+      leds[i] = color;
+    }
+    if(above > NUM_LEDS){ //checks to make sure assignment doesn't go out of bounds. If it does, it resets the board
+      leds(ledPosition,NUM_LEDS) = leds(ledPosition, ledPosition-expSize-1); //still broken. Leaves only a single LED lit
+    } //and will need a check to see whether the counter should go up or down depending on if it will iterate over
+      //the top or below the bottom of the array.
+    else if(below < 0){
+      leds(ledPosition, 0) = leds(ledPosition,ledPosition + expSize);
+    }
+    else{
+      leds(ledPosition, ledPosition-expSize-1) = leds(ledPosition,ledPosition + expSize);
+    }
+    FastLED.show();
+    FastLED.delay(50);
+  }
 }
 
 void fill_rainbow( struct CRGB * pFirstLED, int numToFill,
@@ -269,4 +278,44 @@ void fill_rainbow( struct CRGB * pFirstLED, int numToFill,
         pFirstLED[i] = hsv;
         hsv.hue += deltahue;
     }
+}
+
+void scroll(){
+  newColor = leds; //make copy of initial values
+  int numCycles = 0;
+  int j = 1;
+  while(1){
+    if(j == NUM_LEDS + 1){//if j becomes greater than the amount of LEDS, reset to 1 to take from beginning of array again
+    j = 1;
+    numCycles++;
+    if(numCycles == 10){
+      return;
+    }
+  }
+  for(int i = 0; i <= NUM_LEDS; i++){
+    int diff = i - j;
+    if(diff < 0){ //if the difference is below zero, need to access element that is 'diff' less than number of leds
+      leds[i] = newColor[NUM_LEDS - abs(diff)];
+    }
+    else{
+      leds[i] = newColor[i - j]; //if the difference isn't less than 0 (EX: i = 10, j = 2), simply grab the correct color.
+    }
+  }
+    leds(NUM_LEDS/2,NUM_LEDS-1) = leds(NUM_LEDS/2 - 1 ,0);
+    FastLED.show();
+    FastLED.delay(20);
+    j++; //increment j to reach the next static color
+  }
+}
+
+CRGB colorSelector(int colorNumber, int powerScale){
+        switch(colorNumber){
+        case 0: return CRGB(0,100*(powerScale*.01),0);//red
+        case 1: return CRGB(100*(powerScale*.01),100*(powerScale*.01),0);
+        case 2: return CRGB(100*(powerScale*.01),0,0);//green
+        case 3: return CRGB(100*(powerScale*.01),0,100*(powerScale*.01));
+        case 4: return CRGB(0,0,100*(powerScale*.01));//blue
+        case 5: return CRGB(0,100*(powerScale*.01),100*(powerScale*.01));
+        default: return CRGB(0,0,100*(powerScale*.01));
+      }
 }
