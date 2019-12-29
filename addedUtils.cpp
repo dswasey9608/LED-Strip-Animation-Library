a2243bf6b0fd4fd971d8603c30fd4e1c5f021d5b
BaconSD <dswasey9608@gmail.com>
2019-12-29 00:18:22 -0700

    Added class funcitionality for easy access to certain variables

--- addedUtils.cpp
+++ addedUtils.cpp
@@ -10,7 +10,13 @@
 
 FASTLED_NAMESPACE_BEGIN
 
-CRGB colorSelector(int colorNumber, int powerScale){
+addedUtils::addedUtils(struct CRGB* leds, int numberLEDS, int cycles){
+	this->leds = leds;
+	this->numberLEDS = numberLEDS;
+	this->cycles = cycles;
+}
+
+CRGB addedUtils::colorSelector(int colorNumber, int powerScale){
 	switch(colorNumber){
         case 0: return CRGB(0,100*(powerScale*.01),0);//red
         case 1: return CRGB(100*(powerScale*.01),100*(powerScale*.01),0);
@@ -22,15 +28,7 @@ CRGB colorSelector(int colorNumber, int powerScale){
       }
 }
 
-void colorScale(int powerScale, struct CRGB* leds, struct CRGB* newColor, int numberLEDS){
-	for(int i = 0; i <= numberLEDS; i++){
-		leds[i].r = newColor[i].r * powerScale * 0.01;
-		leds[i].g = newColor[i].g * powerScale * 0.01;
-		leds[i].b = newColor[i].b * powerScale * 0.01;
-	}
-}
-
-void scroll(int cycles, struct CRGB* leds, int numberLEDS){
+void addedUtils::scroll(int cycles){
   struct CRGB newColor[numberLEDS];
   for(int i = 0; i <= numberLEDS; i++){
 	  newColor[i] = leds[i];
@@ -54,14 +52,133 @@ void scroll(int cycles, struct CRGB* leds, int numberLEDS){
 			leds[i] = newColor[diff]; //if the difference isn't less than 0 (EX: i = 10, j = 2), simply grab the correct color.
 		}
 	}
-	for(int i = 0; i <= numberLEDS/ 2; i++){
-		leds[numberLEDS-i] = leds[i];
-	}
+	symmetricStrip();
 	//leds(numberLEDS/2,numberLEDS-1) = leds(numberLEDS/2 - 1 ,0);
     FastLED.show();
     FastLED.delay(20);
     j++; //increment j to reach the next static color
   }
+}
+
+void addedUtils::symmetricStrip(){
+	for(int i = 0; i < (numberLEDS/ 2); i++){
+		leds[numberLEDS-i-1] = leds[i];
+	}
+}
+
+void addedUtils::stackLights(struct CRGB color0, struct CRGB color1, struct CRGB color2){
+  int notLit = numberLEDS - 1; //accounts for odd numbers
+  int currentColor = 0;
+  bool stringFilled = false;
+  bool firstCycle = true;
+  fill_solid(leds, numberLEDS, CRGB(0, 0, 0));
+  while(!stringFilled){
+    for(int i = 0; i <= notLit / 2; i++){
+      switch(currentColor){
+        case 0: leds[i] = color0;
+        break;
+        case 1: leds[i] = color1;
+        break;
+        case 2: leds[i] = color2;
+        break;
+      }
+       //set turquoise
+        if(i-3 >= 0){
+          if(i-3 < (notLit/2) - 2){ //determines how many LEDs to keep up, allowing the stack effect.
+          leds[i-3] = CRGB(0, 0, 0); //turn off anything that is no longer within two spaces behind i
+        }
+      }
+      symmetricStrip();
+      FastLED.show();
+    FastLED.delay(10);
+    }
+    currentColor++;
+    if(currentColor >= 3){
+      currentColor = 0;
+    }
+    FastLED.delay(500);
+    
+    notLit -= 6;
+    if(notLit <= 0){
+      stringFilled = true;
+    }
+  }
+  breathing(true);
+} 
+
+void addedUtils::breathing(){
+  breathing(false);
+}
+
+void addedUtils::breathing(bool external){ //cycles between all primary and secondary colors
+  int powerScale = 0;
+  int increment = 1;
+  bool upIncrement = true;
+  int colorNumber = 0;
+  int numCycles = 0;
+  bool firstCycle = true;
+  bool done = false;
+  struct CRGB* newColor = new struct CRGB[numberLEDS];
+  for(int i = 0; i <= numberLEDS; i++){
+	  newColor[i] = leds[i];
+  }
+  while(1){
+    if(numCycles == cycles + 1){
+      break;
+    }
+    if(!external){
+      struct CRGB singleColor = colorSelector(colorNumber, powerScale);
+      for(int b = 0; b <= numberLEDS; b++){
+        leds[b] = singleColor;
+      }
+    }
+    else{
+      if(firstCycle){
+        powerScale = 100;
+      }
+      colorScale(powerScale, newColor);
+    }
+    FastLED.show();
+      
+    if(powerScale == 100){ //float comparison picks up random values and can never find an equivalence.
+      FastLED.delay(100);
+      upIncrement = false;
+    }
+    if(powerScale == 0){
+      if(!firstCycle){
+        colorNumber++;
+      }
+      if(colorNumber >= 6){
+        colorNumber = 0;
+      }
+      FastLED.delay(200);
+      upIncrement = true;
+      numCycles++;
+    }
+    else{
+      FastLED.delay(20);
+    }
+
+    if(upIncrement){
+      powerScale = powerScale + increment;
+    }
+    else{
+      powerScale = powerScale - increment;
+    }
+    firstCycle = false;
+    //Serial.println("Value of powerScale is: ");
+    //Serial.println(powerScale);
+  }
+  delete[] newColor;
+  
+}
+
+void addedUtils::colorScale(int powerScale, struct CRGB* newColor){
+	for(int i = 0; i <= numberLEDS; i++){
+		leds[i].r = newColor[i].r * powerScale * .01;
+		leds[i].g = newColor[i].g * powerScale * .01;
+		leds[i].b = newColor[i].b * powerScale * .01;
+	}
 }
 
 FASTLED_NAMESPACE_END
--- addedUtils.h
+++ addedUtils.h
@@ -7,17 +7,40 @@
 
 FASTLED_NAMESPACE_BEGIN
 
-CRGB colorSelector(int colorNumber, int powerScale); //choose between a number of colors to assign
+class addedUtils{
+    private:
+        struct CRGB* leds;
+        int numberLEDS;
+        int cycles;
+    public:
+        addedUtils(struct CRGB* leds, int numberLEDS, int cycles);
 
-//Given two Arrays of led information, assign newColor to be the reference (unchanged)
-//and assign new values to leds based on powerScale value assigned
-void colorScale(int powerScale, struct CRGB* leds, struct CRGB* newColor, int numberLEDS);
+        CRGB colorSelector(int colorNumber, int powerScale); //choose between a number of colors to assign
 
-void scrollingRainbow();
+        //Given two Arrays of led information, assign newColor to be the reference (unchanged)
+        //and assign new values to leds based on powerScale value assigned
+        void colorScale(int powerScale, struct CRGB* newColor);
 
-//Takes one array of led values and assigns it to the newColor array. Keeps newColor static
-//and iterates through values of itself assigning to leds one step at a time.
-void scroll(int cycles, struct CRGB* leds, int numberLEDS);
+        void scrollingRainbow();
+
+        //Takes one array of led values and assigns it to the newColor array. Keeps newColor static
+        //and iterates through values of itself assigning to leds one step at a time.
+        void scroll(int cycles);
+
+        //takes current CRGBArray and makes it symmetric from middle outwards (replaces arduino IDE function
+        //that is really simple, but can't find a C++ function for it, so made my own with a simple for loop)
+        void symmetricStrip();
+
+
+        void stackLights(struct CRGB color0, struct CRGB color1, struct CRGB color2);
+
+        void breathing(bool external);
+
+        void breathing();
+
+
+};
+
 
 
 FASTLED_NAMESPACE_END
